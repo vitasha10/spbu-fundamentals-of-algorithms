a043633627990e425508aa7e5bf71f25edd99ac9
Anton Pershin <tony.pershin@gmail.com>
2025-04-12 10:56:01 +0300

    Added plotting as a separate module

--- src/common.py
+++ src/common.py
@@ -1,9 +1,13 @@
 from collections import namedtuple
+from typing import Union
 
 import numpy as np
 from numpy.typing import NDArray
+import networkx as nx
 
 
 ProblemCase = namedtuple("ProblemCase", "input, output")
 NDArrayInt = NDArray[np.int_]
 NDArrayFloat = NDArray[np.float_]
+AnyNxGraph = Union[nx.Graph, nx.DiGraph]
+
